59346813735f343dd029d42315f432aefebb4eff
“kasper341” <kasper_341@hotmail.com>
2016-12-16 16:04:45 -0800

    enrolled user "test2"

--- 500friends-API-tasks/src/enroll.js
+++ 500friends-API-tasks/src/enroll.js
@@ -5,7 +5,7 @@ var request = require("request");
 request({
     url: "http://loyalty-stage.500friends.com/api/enroll.json",
     //Query string
-    qs: {email: "test@test.com", uuid: "GwMlFcW6sZaOQwR"}
+    qs: {email: "test2@test.com", uuid: "GwMlFcW6sZaOQwR", channel: "web"}
 }, function functionName(error, response, body) {
     if(error) {
         console.log(error);
